86260602d1257ba84935150536807bf78a6c1ac4
Konstantin Bläsi <blaesi@amiconsult.de>
2025-06-13 16:57:20 +0200

    .

--- src/+server.ts
+++ src/+server.ts
@@ -1,6 +1,7 @@
 export async function POST({ request }) {
     // no signature check required to demo issue
     const json = await request.json()
+    // json is not always decoded text but sometimes gzip/gibberish/binary
     console.log("Received webhook", json)
     return json({ ok: true })
 }
